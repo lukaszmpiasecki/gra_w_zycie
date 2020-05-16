dd57347e173eaec1c680e93f9ff74c535a7c6173
Piasecki Lukasz <lukasz.piasecki@the-nature-network.pl>
2020-05-16 08:50:31 +0200

    Pierwszy commit

---
+++ mvc/controllers/abstract_controller.py
@@ -0,0 +1,4 @@
+from abc import ABC, abstractmethod
+
+class AbstractCOntroller(ABC):
+    pass
---
+++ mvc/views/board_view.py
@@ -0,0 +1,4 @@
+from abstract_view import AbstractView
+import pygame
+class BoardView(AbstractView):
+    pass
